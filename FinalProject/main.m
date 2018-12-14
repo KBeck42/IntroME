2a382917f3c193a4fcc64115a56889bac70b692e
Kevin Beck <kevin.beck314@gmail.com>
2018-12-13 20:47:54 -0500

    Temporary asv files are gone

--- FinalProject/main.m
+++ FinalProject/main.m
@@ -1,27 +1,33 @@
 try
+    %TODO: take user input to pick file
+    %Open the gcode file
+    fid = fopen('AutoFrost.txt');
+    
     % COM port for the GRBL
     % May need to change COM port
-    s = serial('COM6')
+    s = serial('COM6');
     % Setting baudrate
     set(s,'Baudrate',115200)
     % Open COM port
-    fopen(s)
-    % Pause so GRBL doesn't crap out
+    fopen(s);
+    
+    % Pause so GRBL can catch up
     pause(2);
+    
     % Set some settings in the GRBL
-    fprintf (s, 'G17 G20 G90 G94 G54')
-    % Open the file with the gcode from fusion 360
-    fid = fopen('gcode.txt');
+    fprintf (s, 'G17 G20 G90 G94 G54');
+    
     % Get the first line of the gcode file
     line = fgetl(fid);
     % Print each line of the gcode file to the GRBL
     while ischar(line)
-        fprintf(s, line)
+        fprintf(s, line);
         line = fgetl(fid);
     end
+    
     % Close the gcode file
     fclose(fid);
 catch ME
-    fclose(s)
+    fclose(s);
     ME.message
 end
--- FinalProject/test_motion.m
+++ FinalProject/test_motion.m
@@ -4,9 +4,8 @@ try
     fopen(s)
     pause(2);
     fprintf (s, 'G17 G20 G90 G94 G54')
-    fprintf(s,'G1 Z-2 F15')
-    fprintf(s,'G1 Z0 F15')
-    fclose(s)
+    fprintf(s,'G1 X-2.5 F10')
+    fclose(s);
 catch ME
     fclose(s)
     ME.message
